e9562c101f3fa00a7899cf936d45a87987ccfe7b
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2016-02-05 20:57:25 +0100

    Add BSD License and pyca/cryptography attribution

--- src/_cffi_src/build_hyper.py
+++ src/_cffi_src/build_hyper.py
@@ -1,10 +1,12 @@
 # coding: utf-8
+# Inspired by pyca/cryptography (Apache/BSD)
 
 import os.path
 
 from cffi import FFI
 
 
+# Source: https://github.com/pyca/cryptography/blob/1.2.x/src/_cffi_src/
 with open(os.path.join(
     os.path.dirname(__file__), "cephes_src/cephesx.h"
 )) as f:
