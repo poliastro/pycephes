85f59c98cf366a4b5677a263a4c88c5bb8114004
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2016-03-06 01:42:34 +0100

    Complete setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="pycephes",
     version="0.1.0",
+    description="Python wrapper for CEPHES using CFFI and numba",
+    author="Juan Luis Cano",
+    author_email="hello@juanlu.space",
+    url="https://github.com/poliastro/pycephes",
     packages=find_packages("src"),
     package_dir={"": "src"},
     setup_requires=["cffi>=1.0.0"],
